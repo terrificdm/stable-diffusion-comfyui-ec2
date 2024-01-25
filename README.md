349ab26d1d310b09ba149ba44bec2bb96f1eaa2f
EC2 Default User <ec2-user@ip-172-31-94-189.ec2.internal>
2024-01-25 09:01:21 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# CDK deploy Stable Diffusion Comfyui on EC2   
+
+* Automatically install [Comfyui](https://github.com/comfyanonymous/ComfyUI) on AWS EC2 instance  
+* Expose Comfyui endpoint("ComfyuiPotal" in cloudformation's Outputs) through EC2 public domain name with dedicated port  
+* You need to get ssh key-pair through "GetSSHKeyCommand" in cloudformation's Outputs first if you want to access EC2 instance
+* Default instance type is g5.2xlarge  
+
+# Build  
+* Make sure you follow the [AWS CDK Prerequisites](https://docs.aws.amazon.com/cdk/latest/guide/work-with.html#work-with-prerequisites) before you build the project.
+* Clone this project and change the directory to the root folder of the project, and run below commands:
+```bash
+$ npm install -g aws-cdk
+$ npm install  
+$ cdk bootstrap
+```
+
+# Deploy  
+* Run commands as below:
+```bash
+$ cdk synth
+$ cdk deploy
+```
+
+# Clean  
+```bash
+$ cdk destroy
+```
+
+# Notes  
+* Comfyui files are under /home/ubuntu/ComfyUI directory  
+* Default command in CDK scripts for running SD-Webui is "nohup python main.py --listen --port 8080 > ./sd-comfyui.log 2>&1 &", you can kill that process and run your own  
+* You can use "tail -f /home/ubuntu/ComfyUI/sd-comfyui.log" to get real time logs for comfyui  
+* Regarding how to use Comfyui, read its [official repo](https://github.com/comfyanonymous/ComfyUI)  
---
+++ lib/stable-diffusion-comfyui-ec2-stack.ts
@@ -0,0 +1,80 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+
+export class StableDiffusionComfyuiEc2Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = ec2.Vpc.fromLookup(this, 'VPC', {isDefault: true});
+    
+    const ubuntuLinux = ec2.MachineImage.fromSsmParameter(
+      '/aws/service/canonical/ubuntu/server/22.04/stable/current/amd64/hvm/ebs-gp2/ami-id',
+      { os: ec2.OperatingSystemType.LINUX }
+      );
+    
+    const userData = ec2.UserData.forLinux();
+    userData.addCommands(
+      'apt-get update -y',
+      'apt install gcc -y',
+      `distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\\.//g')`,
+      'wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-keyring_1.0-1_all.deb',
+      'dpkg -i cuda-keyring_1.0-1_all.deb',
+      'apt-get update -y',
+      'apt-get -y install cuda-drivers',
+      'apt-get -y install python3-pip python-is-python3',
+      'pip install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-py3-latest.tar.gz',
+      'mkdir -p /opt/aws/bin',
+      'ln -s /usr/local/bin/cfn-* /opt/aws/bin/',
+      'cd /home/ubuntu',
+      `su ubuntu -c 'git clone https://github.com/comfyanonymous/ComfyUI'`,
+      'cd ComfyUI',
+      `su ubuntu -c 'wget -P models/checkpoints https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors'`,
+      `su ubuntu -c 'wget -P models/checkpoints https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors'`,
+      `su ubuntu -c 'wget -P models/vae https://huggingface.co/madebyollin/sdxl-vae-fp16-fix/resolve/main/sdxl_vae.safetensors'`,
+      `su ubuntu -c 'pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu121'`,
+      `su ubuntu -c 'pip install -r requirements.txt'`,
+      );
+      
+    const keyPair = "comfyui-key-pair.pem";
+    const keyName = keyPair.split(".")[0];
+    const ec2keyPair = new ec2.KeyPair(this, 'KeyPair', {
+      keyPairName: keyName
+    });
+    ec2keyPair.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY)
+    
+    const instance = new ec2.Instance(this, 'Instance', {
+      vpc: vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.G5, ec2.InstanceSize.XLARGE2),
+      machineImage: ubuntuLinux,
+      blockDevices: [{
+        deviceName: '/dev/sda1',
+        volume: ec2.BlockDeviceVolume.ebs(200)   // You can change the disk size as you want
+      }],
+      userData: userData,
+      keyPair: ec2keyPair,
+      init: ec2.CloudFormationInit.fromElements(
+        ec2.InitCommand.shellCommand(`su ubuntu -c 'nohup python main.py --listen --port 8080 > ./sd-comfyui.log 2>&1 &'`),
+        ),
+        resourceSignalTimeout: cdk.Duration.minutes(30),
+      
+    });
+    
+    instance.connections.allowFromAnyIpv4(ec2.Port.tcp(22), 'Allow ssh from internet');
+    instance.connections.allowFromAnyIpv4(ec2.Port.tcp(443), 'Allow https from internet'); // if you don't want to enable "share" flag for directly public access, comment this line out.
+    instance.connections.allowFromAnyIpv4(ec2.Port.tcp(8080), 'Allow access port 8080 from internet'); 
+    
+    new cdk.CfnOutput(this, 'InstanceConsole', {
+      value: 'https://console.aws.amazon.com/ec2/home?region='+instance.env.region+'#Instances:search='+instance.instanceId,
+      description: 'The AWS console for Comfyui EC2 instance'
+    });
+    new cdk.CfnOutput(this, 'GetSSHKeyCommand', {
+      value: `aws ssm get-parameter --name /ec2/keypair/${ec2keyPair.keyPairId} --region ${this.region} --with-decryption --query Parameter.Value --output text > ${keyPair} && chmod 400 ${keyPair}`,
+      description: 'The private key for ssh access EC2 instance'
+    })
+    new cdk.CfnOutput(this, 'ComfyuiPotal', {
+      value: instance.instancePublicDnsName+':8080',
+      description: 'SD-Comfyui access endpoint'
+    })
+  }
+}
